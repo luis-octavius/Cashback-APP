dd6f5d4c68019d19ef251f6e6324a39c36dec7bc
luis-octavius <luisoctavius.sc@gmail.com>
2026-04-15 13:57:02 -0300

    fix: adjust API URL in frontend config

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 window.__APP_CONFIG__ = {
-  API_BASE_URL: "https://cashback-api-production-bceb.up.railway.app/",
+  API_BASE_URL: "https://cashback-api-production-bceb.up.railway.app/api/v1/cashback",
 };
